882023fec83f02ed814db4d2d834a8038a3ec382
pratik2630 <pratikkorgaonkar26@gmail.com>
2023-08-02 15:24:41 +0530

    File read function using annonymous function

--- javascript/filread/index.js
+++ javascript/filread/index.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
 
 // Fileread using Callback function
-
+/*
 function callback(err,data) {
     console.log(data)
+    
 }
 
 fs.readFile("a.txt","utf-8",callback)
+*/
 
+// file read using annonymous function
+fs.readFile("a.txt","utf-8",function callback(err,data) {
+    console.log(data)
+    
+})
